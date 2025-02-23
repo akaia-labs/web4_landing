d8915ef2d9b4d1d2427561a60c6a339a7548e57a
Carina Akaia <cvo.akaia@gmail.com>
2025-02-23 17:44:46 +0000

    Update metadata

--- src/common/config.rs
+++ src/common/config.rs
@@ -6,6 +6,6 @@ pub struct AppMetadata {
 
 pub const APP_METADATA: AppMetadata = AppMetadata {
 	name: "Akaia",
-	description: "R&D e/acc community pushing boundaries of sustainable web.",
+	description: "An R&D e/acc community pushing the boundaries of a self-sovereign and sustainable web.",
 	favicon_url: "https://ipfs.near.social/ipfs/bafkreig5ck3loqtlizliqlfnzyv2qayqf3eqafnael57ur7ny7duon3ak4",
 };
