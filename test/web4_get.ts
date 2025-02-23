607c0c7d0d0d7d87e31193035beb705eb630cdba
Carina Akaia <cvo.akaia@gmail.com>
2025-02-23 16:35:10 +0000

    Update test

--- test/web4_get.ts
+++ test/web4_get.ts
@@ -1,28 +1,47 @@
-const params = new URLSearchParams({
-  "request.json": JSON.stringify({
-    "account_id": null,
-    "path": "/",
-    "params": {},
-    "query": {},
-    "preloads": null,
-  }),
-});
+type SupportedNearProtocolNetwork = "testnet" | "mainnet";
 
-fetch(
-  `https://rpc.web4.testnet.page/account/web4tester.testnet/view/web4_get/${
-    params.toString() ? "?" + params.toString() : ""
-  }`,
-)
-  .then((response) => response.json())
-  .then(({ contentType, body: base64Response }) => {
-    if (contentType === "text/html; charset=UTF-8") {
-      const decodedResponse = atob(base64Response);
+type GetParams = {
+  network?: SupportedNearProtocolNetwork;
+  contractAccountId: string;
+  path?: string;
+};
 
-      console.log(decodedResponse);
-    } else {
-      throw new Error(`Unsupported content type: ${contentType}`);
-    }
-  })
-  .catch((error) => {
-    console.error("Error:", error);
+const get = (
+  { network = "testnet", contractAccountId, path = "/" }: GetParams,
+) => {
+  const params = new URLSearchParams({
+    "request.json": JSON.stringify({
+      account_id: null,
+      path,
+      params: {},
+      query: {},
+      preloads: null,
+    }),
   });
+
+  fetch(
+    `https://rpc.web4.${
+      network === "mainnet" ? "near" : network
+    }.page/account/${contractAccountId}/view/web4_get?${params.toString()}`,
+  )
+    .then((response) => response.json())
+    .then(({ contentType, body: base64Response }) => {
+      if (contentType === "text/html; charset=UTF-8") {
+        const decodedResponse = atob(base64Response);
+
+        console.log(decodedResponse);
+      } else {
+        throw new Error(`Unsupported content type: ${contentType}`);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+const main = () => {
+  // TODO: pass inputs from command line
+  get({ contractAccountId: "web4tester.testnet" });
+};
+
+main();
